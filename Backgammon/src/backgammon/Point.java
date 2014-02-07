f87a9c05053ceb74225af058585e1ef0df15a3f3
Stefano <stefano.forti92@gmail.com>
2014-02-07 10:40:24 +0000

    Update Point.java
    Point commented.

--- Backgammon/src/backgammon/Point.java
+++ Backgammon/src/backgammon/Point.java
@@ -13,20 +13,34 @@ package backgammon;
 public class Point {
     private char colour;
     private int checkersCounter;
-    
+
+/**
+ * It creates an empty point.
+ */   
     public Point (){
         colour = ' ';
         checkersCounter = 0;
     }
     
+  /**
+ * It returns the number of the checkers on a point.
+ */    
     public int countCheckers(){
         return checkersCounter;
     }
     
+  /**
+ * It returns the colour of the checkers on a point.
+ */    
     public char getColour(){
         return colour;
     }
     
+  /**
+ * It sets the colour of the checkers on a point.
+ * @param col it is the colour to be set as the colour of the point
+ * @return It returns -1 if something went wrong, 0 otherwise.
+ */        
     public int setColour(char col){
         int retValue = 0;
         if (isAColour(col)) colour = col;
@@ -37,6 +51,11 @@ public class Point {
        return retValue;
     }
     
+  /**
+ * It sets the number of the checkers on a point.
+ * @param number it is the number to be set for the point
+ * @return It returns -1 if something went wrong, 0 otherwise.
+ */    
     public int setCheckersNumber(int number){
         int retValue = 0;
         if ( number + checkersCounter <= 15 && number >= 0){
@@ -49,7 +68,12 @@ public class Point {
             }
         return retValue;
     }
-    
+     
+ /**
+ * It sets the colour of the checkers on a point.
+ * @param col it is the colour to be set as the colour of the point
+ * @return It returns -1 if something went wrong, 0 otherwise.
+ */   
     public int setPin(char col, int number){
         int retValue = 0;
             if ( this.setColour(col) == -1 ) retValue = -1;
@@ -57,12 +81,21 @@ public class Point {
         return retValue;
     }
     
+  /**
+ * It checks if a point has no checker on.
+ * @return It returns true if there is at least a checker on the point, false otherwise.
+ */    
     public boolean isEmpty(){
         boolean retValue = true;
         if ( checkersCounter != 0) retValue = false;
         return retValue;
     }
     
+  /**
+ * It checks if a character is a colour.
+ * @param c it is the character to be checked.
+ * @return It returns true if a character is a colour, false otherwise.
+ */    
     private boolean isAColour (char c){
         return (c=='B' || c=='b'|| c=='W'||c=='w'|| c==' ');
     }
