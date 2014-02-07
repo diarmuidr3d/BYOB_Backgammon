aa2fdc57a39e2d978e780f26cc0bd26d7b37d2f2
teto1992 <stefano.forti92@gmail.com>
2014-02-07 10:06:27 +0000

    Board Commented

--- Backgammon/src/backgammon/BoardTest.java
+++ Backgammon/src/backgammon/BoardTest.java
@@ -10,7 +10,7 @@ package backgammon;
  *
  * @author BYOB
  */
-public class Backgammon {
+public class BoardTest {
 
     /**
      * @param args the command line arguments
@@ -18,7 +18,23 @@ public class Backgammon {
     public static void main(String[] args) {
         
         Board b = new Board();
+       
+        
+        b.setBoard();
+        b.makeMove(23, 6);
         b.printBoard();
+        
+       /* for (int i = 0; i<10; i++){
+           a = b.rollDice();
+           System.out.println( a[0] + " " + a[1]);
+        }*/
+        
+       
+        
+       b.makeMove(0, 6);
+       b.printBoard();
+      
     }
-    
 }
+    
+
---
+++ Backgammon/src/backgammon/Point.java
@@ -0,0 +1,71 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package backgammon;
+
+/**
+ *
+ * @author BYOB
+ */
+public class Point {
+    private char colour;
+    private int checkersCounter;
+    
+    public Point (){
+        colour = ' ';
+        checkersCounter = 0;
+    }
+    
+    public int countCheckers(){
+        return checkersCounter;
+    }
+    
+    public char getColour(){
+        return colour;
+    }
+    
+    public int setColour(char col){
+        int retValue = 0;
+        if (isAColour(col)) colour = col;
+            else { 
+                System.out.println("setPin: Wrong value for colour.");
+                retValue = -1;
+            }
+       return retValue;
+    }
+    
+    public int setCheckersNumber(int number){
+        int retValue = 0;
+        if ( number + checkersCounter <= 15 && number >= 0){
+                checkersCounter = number;
+                if ( checkersCounter == 0 ) colour = ' ';
+            }
+            else {
+                System.out.println("setPin: Wrong value for number.");
+                retValue = -1;
+            }
+        return retValue;
+    }
+    
+    public int setPin(char col, int number){
+        int retValue = 0;
+            if ( this.setColour(col) == -1 ) retValue = -1;
+            if ( this.setCheckersNumber(number) == -1 ) retValue = -1;
+        return retValue;
+    }
+    
+    public boolean isEmpty(){
+        boolean retValue = true;
+        if ( checkersCounter != 0) retValue = false;
+        return retValue;
+    }
+    
+    private boolean isAColour (char c){
+        return (c=='B' || c=='b'|| c=='W'||c=='w'|| c==' ');
+    }
+
+}
+
