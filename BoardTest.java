94645a0ae2d70379fceb570baede6a99b46a1c42
Diarmuid <diarmuidr3d@gmail.com>
2014-02-09 15:07:28 +0000

    Updates by Michael to MakeMove and BoardTest

--- BoardTest.java
+++ BoardTest.java
@@ -11,30 +11,69 @@ package backgammon;
  * @author BYOB
  */
 public class BoardTest {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        
-        Board b = new Board();
-       
-        
-        b.setBoard();
-        b.makeMove(23, 6);
-        b.printBoard();
-        
-       /* for (int i = 0; i<10; i++){
-           a = b.rollDice();
-           System.out.println( a[0] + " " + a[1]);
-        }*/
-        
-       
-        
-       b.makeMove(20, b.WHITE_OFF);
-       b.printBoard();
-      
-    }
+	public static void main(String[] args) {
+		Board b = new Board();
+		System.out.println("checks the display of the board");
+		b.setBoard();
+		b.printBoard();
+		
+		System.out.println("checks if blacks can move onto each other");
+		b.makeMove(12,5);
+		b.printBoard();
+		
+		System.out.println("checks whites can move onto each other");
+		b.makeMove(16,18);
+		b.printBoard();
+		
+		System.out.println("checks if whites can move onto a set of blacks(nothing should happen)");
+		b.makeMove(0,5);
+		b.printBoard();
+		
+		System.out.println("checks if blacks can move onto a set of whites(nothing should happen)");
+		b.makeMove(23,18);
+		b.printBoard();
+		
+		System.out.println("checks if whites can move onto an open pin");
+		b.makeMove(0,1);
+		b.printBoard();
+		
+		System.out.println("checks if blacks can move onto an open pin");
+		b.makeMove(23,22);
+		b.printBoard();
+		
+		System.out.println("checks if white piece can be knocked onto the bar");
+		b.makeMove(5,0);
+		b.printBoard();
+		
+		System.out.println("checks if black piece can be knocked onto the bar");
+		b.makeMove(18,23);
+		b.printBoard(); 
+		
+		System.out.println("checks if white piece can move from the bar");
+		b.makeMove(Board.WHITE_BAR,21);
+		b.printBoard();
+		
+		System.out.println("checks if black piece can move from the bar");
+		b.makeMove(Board.BLACK_BAR,4);
+		b.printBoard();
+		
+		System.out.println("checks if white piece can bear off");
+		b.makeMove(18,Board.WHITE_OFF);
+		b.printBoard();
+		
+		System.out.println("checks if black piece can bear off");
+		b.makeMove(5,Board.BLACK_OFF);
+		b.printBoard();
+		
+		System.out.println("checks if resets Board");
+		b.setBoard();
+		b.printBoard();
+		
+		int a[];
+		System.out.println("checks rollDice method");
+		a = b.rollDice();
+		for(int i=0;i<2;i++) {
+		System.out.println("Dice"+(i+1)+" "+a[i]);
+		}
+	}
 }
-    
-
