1345b80a0bda0277222089c0142ddace65e62647
teto1992 <stefano.forti92@gmail.com>
2014-02-16 11:00:42 +0000

    Merge remote-tracking branch 'backgammon/master'

---
+++ Documents/GitHub/src/backgammon/HumanPlayer.java
@@ -0,0 +1,31 @@
+/*
+ * COMP20050 - Software Engineering Project 2 - 2014
+ * Team: BYOB
+ * Members: Michael Dalton (12328661), Stefano Forti(13201749), Diarmuid Ryan (11363776)
+ */
+
+package backgammon;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author BYOB
+ */
+public class HumanPlayer {
+    
+     public int readMove(){
+        Scanner input = new Scanner(System.in);
+        
+        while(input.hasNext()){
+            
+        }
+        return 0;
+}
+
+    
+    public int playerMove(){
+        //Diarmuid
+        return 0;
+    }
+ }
