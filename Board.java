45a0c8ef9b4cf0d450cab73b41b7e18f2e68b11e
MichaelDalton7 <michaeljohndalton@ymail.com>
2014-02-24 02:03:27 +0000

    Added a getColour()
    Someone accidentally copied and pasted their HumanPlayer class over everything so I had to copy and paste my Board class over everything which includes a new getColour method

--- Board.java
+++ Board.java
@@ -1,210 +1,332 @@
 /*
+ /*
  * COMP20050 - Software Engineering Project 2 - 2014
  * Team: BYOB
  * Members: Michael Dalton (12328661), Stefano Forti(13201749), Diarmuid Ryan (11363776)
  */
-package backgammon;
 
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.util.Scanner;
+import java.util.Random;
 
 /**
  *
  * @author BYOB
  */
-public class HumanPlayer {
+public class Board {
+
+    Point[] boardPins = new Point[24]; // 0-23 pins are the actual pins. Setting refers to: http://en.wikipedia.org/wiki/File:Bg-movement.svg
+    /* Counters for the bar and the off */
+    int whiteBar;
+    int blackBar;
+    int whiteOff;
+    int blackOff;
+    char playerTurn = 'W';
+    /**
+     * This constant represents the White Off.
+     */
+    public static final int WHITE_OFF = 24;
+    /**
+     * This constant represents the Black Off.
+     */
+    public static final int BLACK_OFF = 25;
+    /**
+     * This constant represents the White Bar.
+     */
+    public static final int WHITE_BAR = 26;
+    /**
+     * This constant represents the Black Bar.
+     */
+    public static final int BLACK_BAR = 27;
 
     /**
-     * It reads one, two, three or four moves from the input and returns them in an array.
-     * The function tolerates some mistakes.
-     * @param b it is the actual board
-     * @return It returns a int[][] moves where  moves[i][0] is the source point and moves[i][1] is the destination point. 
-     * Those parameters are computed to be the input of board.makeMove()
+     * Sets an empty board.
      */
-    public int[][] readMoves(Board b) throws FileNotFoundException {
+    public Board() {
+        for (int j = 0; j < 24; j++) {
+            boardPins[j] = new Point();
+        }
+    }
 
-        Scanner input;
-        int[][] movesArray = null;
-        int i, j;
-        boolean validMoveFound = false;
+    public char getTurn() {
+        return playerTurn;
+    }
 
-        String inputMoves;
+    public void setTurn(char playerChar) {
+        playerTurn = playerChar;
+    }
 
-        input = new Scanner(System.in);
+    /**
+     * Sets the board to start a game.
+     * <p>
+     * White and black checkers are put in their initial position by this method
+     * <p>
+     */
+    public void setBoard() {
+        for (int j = 0; j < 24; j++) {
+            boardPins[j].setPin(' ', 0);
+        }
+        boardPins[0].setPin('W', 2);
+        boardPins[23].setPin('B', 2);
+        boardPins[5].setPin('B', 5);
+        boardPins[18].setPin('W', 5);
+        boardPins[7].setPin('B', 3);
+        boardPins[16].setPin('W', 3);
+        boardPins[11].setPin('W', 5);
+        boardPins[12].setPin('B', 5);
 
-        while (!validMoveFound && input.hasNextLine()) {
-            inputMoves = input.nextLine();
-            inputMoves = inputMoves.trim();
+        whiteBar = 0;
+        blackBar = 0;
+        whiteOff = 0;
+        blackOff = 0;
+    }
 
-            if (!inputMoves.matches("(([1-9]([0-9]{0,1})(\\s)*-(\\s)*[1-9]([0-9]{0,1})\\s+){0,3}[1-9]([0-9]{0,1})(\\s)*-(\\s)*[1-9]([0-9]{0,1}))")) {
-                System.out.println("Invalid: " + inputMoves);
+    private String printTopOfBoard() {
+        String topofboard = "";
+        for (int i = 12; i <= 17; i++) {
+            if (boardPins[i].countCheckers() > 0) {
+                topofboard = topofboard + boardPins[i].countCheckers() + boardPins[i].getColour() + "  ";
             } else {
-                validMoveFound = true;
-                inputMoves = inputMoves.replaceAll("\\s+", " ");
-                inputMoves = inputMoves.replaceAll("\\s*-\\s*", "-");
-                String[] splittedMoves = inputMoves.split(" ");
-                movesArray = new int[splittedMoves.length][2];
-
-                i = 0;
-
-                for (String s : splittedMoves) {
-                    String[] splittedValues = s.split("-");
-                    j = 0;
-                    for (String x : splittedValues) {
-                        movesArray[i][j] = Integer.parseInt(x);
-                        j++;
-                    }
-                    i++;
-                }
+                topofboard = topofboard + "|   ";
             }
         }
-        if (movesArray != null) {
-            if (b.getTurn() == 'W') {
-                for (i = 0; i < movesArray.length; i++) {
-                    movesArray[i][0]--;
-                    if (movesArray[i][0] == 24) {
-                        movesArray[i][0] = Board.WHITE_BAR;
-                        movesArray[i][1] = movesArray[i][1] - 1;
-                    } else if (movesArray[i][0] + movesArray[i][1] > 24) {
-                        movesArray[i][1] = Board.WHITE_OFF;
-                    } else {
-                        movesArray[i][1] = movesArray[i][0] + movesArray[i][1];
-                    }
-                }
-            } else if (b.getTurn() == 'B') {
-                for (i = 0; i < movesArray.length; i++) {
-                    movesArray[i][0]--;
-                    if (movesArray[i][0] == 24) {
-                        movesArray[i][0] = Board.BLACK_BAR;
-                        movesArray[i][1] = 25 - movesArray[i][1];
-                    } else if (movesArray[i][0] - movesArray[i][1] < 0) {
-                        movesArray[i][1] = Board.BLACK_OFF;
-                    } else {
-                        movesArray[i][1] = movesArray[i][0] - movesArray[i][1];
-                    }
-                }
-            }
-
+        if (blackBar == 0) {
+            topofboard = topofboard + "    ";
+        } else {
+            topofboard = topofboard + blackBar + "B  ";
         }
-
-        return movesArray;
-
+        for (int i = 18; i <= 23; i++) {
+            if (boardPins[i].countCheckers() > 0) {
+                topofboard = topofboard + boardPins[i].countCheckers() + boardPins[i].getColour() + "  ";
+            } else {
+                topofboard = topofboard + "|   ";
+            }
+        }
+        if (whiteOff == 0) {
+            topofboard = topofboard + "   ";
+        } else {
+            topofboard = topofboard + whiteOff + "W ";
+        }
+        return topofboard;
     }
 
-    public int quitGame() {
-        String response = "";
-        Scanner input = new Scanner(System.in);
-        System.out.println("Would you like to exit the game: (y/n)\n");
-        do {
-            response = input.nextLine();
-            response = response.trim();
-            response = response.toLowerCase();
-            if ((!response.equals("y")) && (!response.equals("n"))) {
-                System.out.println("You have entered an invalid character, Please try again\n");
+    private String printBottomOfBoard() {
+        String bottomofboard = "";
+        for (int i = 11; i >= 6; i--) {
+            if (boardPins[i].countCheckers() > 0) {
+                bottomofboard = bottomofboard + boardPins[i].countCheckers() + boardPins[i].getColour() + "  ";
+            } else {
+                bottomofboard = bottomofboard + "|   ";
             }
-        } while ((!response.equals("y")) && (!response.equals("n")));
-        if (response.equals("y")) {
-            System.out.println("Thank you for playing\n");
-            System.exit(0);
         }
-        System.out.println("You have choosen to continue playing\n");
-        input.close();
-        return 0;
+        if (whiteBar == 0) {
+            bottomofboard = bottomofboard + "    ";
+        } else {
+            bottomofboard = bottomofboard + whiteBar + "W  ";
+        }
+        for (int i = 5; i >= 0; i--) {
+            if (boardPins[i].countCheckers() > 0) {
+                bottomofboard = bottomofboard + boardPins[i].countCheckers() + boardPins[i].getColour() + "  ";
+            } else {
+                bottomofboard = bottomofboard + "|   ";
+            }
+        }
+        if (blackOff == 0) {
+            bottomofboard = bottomofboard + "    ";
+        } else {
+            bottomofboard = bottomofboard + blackOff + "B ";
+        }
+        return bottomofboard;
     }
 
-    public int playerMove(Board b, int[] diceRoll) throws FileNotFoundException, IOException {
+    /**
+     * Prints the actual configuation of the board. It uses printTopOfBoard()
+     * and printBottomOfBoard() to create the strings for the top and bottom of
+     * the board respectively.
+     */
+    public void printBoard() {
+        System.out.println("13--+---+---+---+---18  BAR 19--+---+---+---+---24  OFF");
+        System.out.println(printTopOfBoard());
+        System.out.println("\n");
+        System.out.println(printBottomOfBoard());
+        System.out.println("12--+---+---+---+---07  BAR 06--+---+---+---+---01  OFF");
+        System.out.println("\n\n");
+    }
+
+    /**
+     * Rolls the dice. (1)
+     * <p>
+     * The method generates two random numbers when called.
+     * <p>
+     * @return Returns an int array a[2] containing the two random numbers.
+     */
+    public int[] rollDice() {
+        int[] a = new int[2];
+        Random randomGenerator = new Random();
+        a[0] = randomGenerator.nextInt(6) + 1;
+        a[1] = randomGenerator.nextInt(6) + 1;
+        return a;
+    }
+
+    /**
+     * Makes the specified move from the black bar to a destination.
+     * <p>
+     * @param destination It specifies the number of the destination in
+     * boardPins[]
+     */
+    private int moveFromBlackBar(int destination) {
         int retVal = 0;
-        int movesCounter;
-
-        switch (b.getTurn()) {
-            case 'W':
-                if (b.isADoubleRoll(diceRoll)) {
-                    movesCounter = 4;
-
-                    while (movesCounter != 0) {
-                        System.out.println(b.getTurn() + ":Double " + diceRoll[1] + "! Input your " + movesCounter + " moves in the format startPoint-steps: ");
-
-                        int[][] moves = readMoves(b);
-
-                        if (moves != null) {
-                            for (int[] move : moves) {
-                                if (b.makeMove(move[0], move[1]) != -1) {
-                                    movesCounter--;
-                                }
-                            }
-                        }
-                    }
-                } else {
-
-                    movesCounter = 2;
-
-                    while (movesCounter != 0) {
-
-                        System.out.println(b.getTurn() + "You got " + diceRoll[0] + "," + diceRoll[1] + "! Input your " + movesCounter + " moves in the format startPoint-steps: ");
-
-                        int[][] moves = readMoves(b);
-                        if (moves != null) {
-                            for (int[] move : moves) {
-                                if (b.makeMove(move[0], move[1]) != -1) {
-                                    movesCounter--;
-                                }
-                            }
-                        }
-
-                    }
-
-                }
-                b.setTurn('B');
-                break;
-            case 'B':
-                if (b.isADoubleRoll(diceRoll)) {
-                    movesCounter = 4;
-
-                    while (movesCounter != 0) {
-                        System.out.println(b.getTurn() + "Double " + diceRoll[1] + "! Input your " + movesCounter + " moves in the format startPoint-steps: ");
-
-                        int[][] moves = readMoves(b);
-
-                        if (moves != null) {
-
-                            for (int[] move : moves) {
-                                if (b.makeMove(move[0], move[1]) != -1) {
-                                    movesCounter--;
-                                }
-                            }
-                        }
-                    }
-                } else {
-
-                    movesCounter = 2;
-
-                    while (movesCounter > 0) {
-
-                        System.out.println(b.getTurn() + "You got " + diceRoll[0] + "," + diceRoll[1] + "! Input your " + movesCounter + " moves in the format startPoint-steps: ");
-
-                        int[][] moves = readMoves(b);
-                        if (moves != null) {
-                            for (int[] move : moves) {
-                                if (b.makeMove(move[0], move[1]) != -1) {
-                                    movesCounter--;
-                                }
-                            }
-                        }
-
-                    }
-
-                }
-                b.setTurn('W');
-                break;
-
+        char b = 'B';
+        char w = 'W';
+        if ((boardPins[destination].countCheckers() < 2) && (boardPins[destination].getColour() == w)) {
+            blackBar--;
+            whiteBar++;
+            boardPins[destination].setPin(b, (boardPins[destination].countCheckers()));
+            retVal = 0;
+        } else if ((boardPins[destination].getColour() == b) || (boardPins[destination].getColour() == ' ')) {
+            blackBar--;
+            boardPins[destination].setPin(b, (boardPins[destination].countCheckers() + 1));
+            retVal = 0;
+        } else if ((boardPins[destination].countCheckers() >= 2) && (boardPins[destination].getColour() == w)) {
+            System.out.println("invalid Move");
+            retVal = -1;
         }
 
         return retVal;
     }
 
-    public boolean quitQuestion() {
-        return true;
+    /**
+     * Makes the specified move from the white bar to a destination.
+     * <p>
+     * @param destination It specifies the number of the destination in
+     * boardPins[]
+     */
+    private int moveFromWhiteBar(int destination) {
+        int retVal = 0;
+        char b = 'B';
+        char w = 'W';
+        if ((boardPins[destination].countCheckers() < 2) && (boardPins[destination].getColour() == b)) {
+            whiteBar--;
+            blackBar++;
+            boardPins[destination].setPin(w, (boardPins[destination].countCheckers()));
+            retVal = 0;
+        } else if ((boardPins[destination].getColour() == w) || (boardPins[destination].getColour() == ' ')) {
+            whiteBar--;
+            boardPins[destination].setPin(w, (boardPins[destination].countCheckers() + 1));
+            retVal = 0;
+        } else if ((boardPins[destination].countCheckers() >= 2) && (boardPins[destination].getColour() == b)) {
+            System.out.println("invalid Move");
+            retVal = -1;
+        }
+        return retVal;
+    }
+
+    /**
+     * Makes the specified move from a source to a destination.
+     * <p>
+     * @param source It specifies the number of the source point in boardPins[]
+     * @param destination It specifies the number of the destination in
+     * boardPins[]
+     * @return It returns 0 if everything went well, -1 otherwise.
+     */
+    public int makeMove(int source, int destination) {
+
+        int retVal = 0;
+        //from black bar
+        if (source == BLACK_BAR) {
+            retVal = moveFromBlackBar(destination);
+
+        } //from white bar
+        else if (source == WHITE_BAR) {
+            retVal = moveFromWhiteBar(destination);
+        } //empty source!
+        else if (boardPins[source].countCheckers() == 0) {
+            System.out.println("Nothing on the source pin");
+            retVal = -1;
+        } //to white off
+        else if (destination == WHITE_OFF) {
+            whiteOff++;
+            boardPins[source].setPin('W', (boardPins[source].countCheckers() - 1));
+            retVal = 0;
+        } //to black off
+        else if (destination == BLACK_OFF) {
+            blackOff++;
+            boardPins[source].setPin('B', (boardPins[source].countCheckers() - 1));
+            retVal = 0;
+        } //same colour move or empty destination
+        else if ((boardPins[source].getColour() == boardPins[destination].getColour()) || (boardPins[destination].isEmpty())) {
+            boardPins[destination].setPin(boardPins[source].getColour(), (boardPins[destination].countCheckers() + 1));
+            boardPins[source].setPin(boardPins[source].getColour(), (boardPins[source].countCheckers() - 1));
+            retVal = 0;
+        } //eating move
+        else if (boardPins[destination].countCheckers() == 1) {
+            if (boardPins[destination].getColour() == 'W') {
+                whiteBar++;
+            } else if (boardPins[destination].getColour() == 'B') {
+                blackBar++;
+            }
+            boardPins[destination].setPin(boardPins[source].getColour(), 1);
+            boardPins[source].setPin(boardPins[source].getColour(), (boardPins[source].countCheckers() - 1));
+            retVal = 0;
+        }
+        else{
+        	System.out.println("Invalid Move");
+        	retVal = -1;
+        }
+        return retVal;
+    }
+    
+    public char getColour(int position){
+        return boardPins[position].getColour();
+    }
+
+
+    public int[] firstPlay() {
+        int[] diceRoll = rollDice();
+        boolean endFirstPlay = false;
+
+        while(!endFirstPlay){
+            if(diceRoll[0] > diceRoll[1]){
+                this.setTurn('W');
+                endFirstPlay = true;
+            } 
+            else if(diceRoll[0] < diceRoll[1]){
+                this.setTurn('B');
+                endFirstPlay = true;
+            }
+            else{
+            	diceRoll = rollDice();
+            }
+        }
+        return diceRoll;
+    }
+
+    public boolean whiteCheckForBearOff() {
+        int count = 0;
+        for (int j = 18; j <= 23; j++) {
+            if (boardPins[j].getColour() == 'W') {
+                count = count + boardPins[j].countCheckers();
+            }
+        }
+        count = count + whiteOff;
+        return count == 15;
+    }
+
+    public boolean blackCheckForBearOff() {
+        int count = 0;
+        for (int j = 0; j <= 5; j++) {
+            if (boardPins[j].getColour() == 'B') {
+                count = count + boardPins[j].countCheckers();
+            }
+        }
+        count = count + blackOff;
+        return count == 15;
+    }
+    
+    public boolean checkBearOff(char player){
+        if (player == 'W') return whiteCheckForBearOff();
+        else return blackCheckForBearOff();
+    }
+    
+    public boolean isADoubleRoll(int[] diceRoll){
+        return diceRoll[0] == diceRoll[1];
     }
 }
-
