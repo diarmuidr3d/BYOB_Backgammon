013a85dd575756adcf3e37cbd998261626595ffe
Stefano <stefano.forti92@gmail.com>
2014-02-24 09:58:56 +0000

    Last playerMove()
    Questions to the user

--- HumanPlayer.java
+++ HumanPlayer.java
@@ -4,228 +4,92 @@
  * Members: Michael Dalton (12328661), Stefano Forti(13201749), Diarmuid Ryan (11363776)
  */
 
+package backgammon;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintStream;
 import java.util.Scanner;
 
 /**
  *
- * @author BYOB
+ * @author Stefano
  */
-public class HumanPlayer {
-	
-	public int quitGame(){
-		String response = "";
-		Scanner input = new Scanner(System.in);
-		System.out.println("Would you like to exit the game: (y/n)");
-		do{
-			response = input.nextLine();
-			response = response.trim();
-			response = response.toLowerCase();
-			if((!response.equals("y")) && (!response.equals("n"))){
-				System.out.println("You have entered an invalid character, Please try again");
-			}
-		}while((!response.equals("y")) && (!response.equals("n")));
-		if(response.equals("y")){
-			System.out.println("Thank you for playing\n");
-			System.exit(0);
-		}
-		System.out.println("You have choosen to continue playing\n");
-		return 0;
-	}
-	
-	public int passTurn(Board b){
-		String response = "";
-		Scanner input = new Scanner(System.in);
-		System.out.println("Would you like to pass your turn: (y/n)");
-		do{
-			response = input.nextLine();
-			response = response.trim();
-			response = response.toLowerCase();
-			if((!response.equals("y")) && (!response.equals("n"))){
-				System.out.println("You have entered an invalid character, Please try again");
-			}
-		}while((!response.equals("y")) && (!response.equals("n")));
-		if(response.equals("y")){
-			System.out.println("You have choosen to pass your turn\n");
-			if(b.getTurn() == 'W'){
-				b.setTurn('B');
-			}
-			else{
-				b.setTurn('W');
-			}
-		}
-		else{
-			System.out.println("You have choosen to not pass your turn\n");
-		}
-		return 0;
-	}
+public class HumanPlayerTest {
 
-	public int[][] readMoves(Board b) throws FileNotFoundException {
-
-        Scanner input;
-        int[][] movesArray = null;
-        int i, j;
-        boolean validMoveFound = false;
-
-        String inputMoves;
-
-        input = new Scanner(System.in);
-
-        while (!validMoveFound && input.hasNextLine()) {
-            inputMoves = input.nextLine();
-            inputMoves = inputMoves.trim();
-
-            if (!inputMoves.matches("(([1-9]([0-9]{0,1})(\\s)*-(\\s)*[1-9]([0-9]{0,1})\\s+){0,3}[1-9]([0-9]{0,1})(\\s)*-(\\s)*[1-9]([0-9]{0,1}))")) {
-                System.out.println("Invalid: " + inputMoves);
-            } else {
-                validMoveFound = true;
-                inputMoves = inputMoves.replaceAll("\\s+", " ");
-                inputMoves = inputMoves.replaceAll("\\s*-\\s*", "-");
-                String[] splittedMoves = inputMoves.split(" ");
-                movesArray = new int[splittedMoves.length][2];
-
-                i = 0;
-
-                for (String s : splittedMoves) {
-                    String[] splittedValues = s.split("-");
-                    j = 0;
-                    for (String x : splittedValues) {
-                        movesArray[i][j] = Integer.parseInt(x);
-                        j++;
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws FileNotFoundException, IOException {
+           
+            //Testing the readMoves() method
+                PrintStream psOut = null;
+                PrintStream stdOut = System.out;
+                InputStream stdIn = System.in;
+                Board b = new Board();
+                b.setTurn('W');
+                
+                
+                FileInputStream sIn = null;
+                boolean test = true;
+                HumanPlayer p = new HumanPlayer();
+                int i;
+                
+                if (test){
+                    psOut = new PrintStream(".\\test\\outputTest.txt");
+                    System.setOut(psOut);
+                } 
+                for (i=0; i<3; i++){
+                    sIn = new FileInputStream(".\\test\\inputTest"+i+".txt");
+                    System.out.println("test " + i);
+                    System.setIn(sIn); 
+                    int moves[][] = p.readMoves(b);
+                    if(moves!=null)
+                    for (int move[] : moves){
+                        System.out.println(move[0] + " " + move[1]);
                     }
-                    i++;
                 }
-            }
-        }
-        if (movesArray != null) {
-            if(b.getTurn() == 'W') {
-                for(i = 0; i < movesArray.length; i++) {
-                    movesArray[i][0]--;
-                    if (movesArray[i][0] == 24) {
-                        movesArray[i][0] = Board.WHITE_BAR;
-                        movesArray[i][1] = movesArray[i][1] - 1;
-                    } 
-                    else if (movesArray[i][0] + movesArray[i][1] > 24){
-                        movesArray[i][1] = Board.WHITE_OFF;
-                    } 
+                
+                if(psOut != null) psOut.close();
+                if (sIn != null) sIn.close();
+                
+                System.setOut(stdOut);
+                System.setIn(stdIn);
+                
+                Scanner actualOut = new Scanner(new BufferedReader(new FileReader(".\\test\\outputTest.txt")));
+                Scanner correctOut = new Scanner(new BufferedReader(new FileReader(".\\test\\outputCheck.txt")));
+
+                String s1, s2;
+                int k = 0;
+                while (correctOut.hasNextLine() || actualOut.hasNextLine()){
+                    k++;
+                    s1 = correctOut.nextLine();
+                    s2 = actualOut.nextLine();
+                    if (!s1.equals(s2)) 
+                        System.err.println("test failed at line " + k + " actualOutput: " + s2 + " correctOutput: " + s1);
                     else{
-                        movesArray[i][1] = movesArray[i][0] + movesArray[i][1];
+                        System.out.println("line OK " + k + " output: " + s1);
                     }
                 }
-            }
-            else if (b.getTurn() == 'B'){
-                for(i = 0; i < movesArray.length; i++){
-                    movesArray[i][0]--;
-                    if (movesArray[i][0] == 24) {
-                        movesArray[i][0] = Board.BLACK_BAR;
-                        movesArray[i][1] = 24 - movesArray[i][1];
-                    } 
-                    else if(movesArray[i][0] - movesArray[i][1] < 0){
-                        movesArray[i][1] = Board.BLACK_OFF;
-                    } 
-                    else{
-                        movesArray[i][1] = movesArray[i][0] - movesArray[i][1];
-                    }
-                }
-            }
-
-        }
-
-        return movesArray;
+                
+                if (k == 14) System.out.println("readMoves(): Test Passed!");
+                
+                //end of the readMoves() method test
+                
+                b.setBoard();
+                b.setTurn('W');
+                int dice[] = {1,3};
+                p.playerMove(b, dice);
+                b.printBoard();
+                p.playerMove(b, dice);
+                b.printBoard();
+                int dice1[] = {6,6};
+                p.playerMove(b, dice1);
+                b.printBoard();
     }
     
-    public int playerMove(Board b, int[] diceRoll) throws FileNotFoundException, IOException {
-        int retVal = 0;
-        int movesCounter;
-
-        switch (b.getTurn()) {
-            case 'W':
-            	System.out.println("it's whites turn");
-                if (b.isADoubleRoll(diceRoll)) {
-                    movesCounter = 4;
-
-                    while(movesCounter > 0){
-                        System.out.println("Double " + diceRoll[1] + "! Input your " + movesCounter + " moves in the format startPoint-steps: ");
-
-                        int[][] moves = readMoves(b);
-
-                        if ((moves != null) && (moves.length <= movesCounter)){
-                            for (int[] move : moves) {
-                                if (b.makeMove(move[0], move[1]) != -1) {
-                                    movesCounter--;
-                                }
-                            }
-                        }
-                    }
-                } else {
-
-                    movesCounter = 2;
-
-                    while (movesCounter > 0) {
-
-                        System.out.println("You got " + diceRoll[0] + "," + diceRoll[1] + "! Input your " + movesCounter + " moves in the format startPoint-steps: ");
-
-                        int[][] moves = readMoves(b);
-                        if ((moves != null) && (moves.length <= movesCounter)) {
-                            for (int[] move : moves) {
-                               if( b.makeMove(move[0], move[1])!= -1 )
-                                movesCounter--;
-                            }
-                        }
-
-                    }
-
-                }
-                b.setTurn('B');
-                break;
-            case 'B':
-            	System.out.println("it's blacks turn");
-                if (b.isADoubleRoll(diceRoll)) {
-                    movesCounter = 4;
-
-                    while (movesCounter > 0) {
-                        System.out.println("Double " + diceRoll[1] + "! Input your " + movesCounter + " moves in the format startPoint-steps: ");
-
-                        int[][] moves = readMoves(b);
-
-                        if ((moves != null) && (moves.length <= movesCounter)) {
-                            for (int[] move : moves) {
-                                if (b.makeMove(move[0], move[1]) != -1) {
-                                    movesCounter--;
-                                }
-                            }
-                        }
-                    }
-                } else {
-
-                    movesCounter = 2;
-
-                    while (movesCounter > 0) {
-
-                        System.out.println("You got " + diceRoll[0] + "," + diceRoll[1] + "! Input your " + movesCounter + " moves in the format startPoint-steps: ");
-
-                        int[][] moves = readMoves(b);
-                        if ((moves != null) && (moves.length <= movesCounter)) {
-                            for (int[] move : moves) {
-                                
-                                if ( b.makeMove(move[0], move[1])!=-1)
-                                movesCounter--;
-                            }
-                        }
-
-                    }
-
-                }
-                b.setTurn('W');
-                break;
-
-        }
-
-        return retVal;
-    }
-
-    public boolean quitQuestion() {
-        return true;
-    }
 }
