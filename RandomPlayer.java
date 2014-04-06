4b409e8bb47f575946436e0bf2cd87ae08806396
MichaelDalton7 <michaeljohndalton@ymail.com>
2014-04-06 23:19:40 +0100

    Implemented setPlayerColour() and some additions

--- RandomPlayer.java
+++ RandomPlayer.java
@@ -6,6 +6,10 @@
 
 package backgammon;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.List;
+
 public class RandomPlayer{
 
     private char playerColour;
@@ -15,7 +19,31 @@ public class RandomPlayer{
 
     }
     
-    public void getPlay(){
+    /**
+     * Sets the player colour
+     *
+     * @param c is the colour to be set
+     */
+    public void setPlayerColour(char c) {
+        playerColour = c;
+    }
+    
+    public void getPlay(HumanPlayer player,Dice d, Board b) throws FileNotFoundException, IOException{
+    	List<int[]> possible_moves;
+    	possible_moves = b.allPossiblePlays(d,b);
+    	for (int i = 0; i < possible_moves.size(); i++) {
+    		System.out.println("Play option "+possible_moves.get(i)[0]+": "+possible_moves.get(i)[1]+"-"+possible_moves.get(i)[2]);
+    	}
+    	if(!d.isDoubleRoll()){
+    		int moves1[][] = new int [2][2];
+    		
+    		//player.computeMoves(moves1[][], b);
+    	}
+    	else{
+    		int moves1[][] = new int [4][2];
+    		
+    		//player.computeMoves(moves1[][], b);
+    	}
     	
     	
     }
