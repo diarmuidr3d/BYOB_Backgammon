cae6162dc10051e9a98807e009a84e3a8dcb2ec5
Diarmuid Ryan <diarmuidr3d@gmail.com>
2014-04-06 18:28:25 +0100

    Correct file extensions for BgOnePlayer and RandomPlayer

---
+++ RandomPlayer.java
@@ -0,0 +1,22 @@
+/*
+ * COMP20050 - Software Engineering Project 2 - 2014
+ * Team: BYOB
+ * Members: Michael Dalton (12328661), Stefano Forti(13201749), Diarmuid Ryan (11363776)
+ */
+
+package backgammon;
+
+public class RandomPlayer{
+
+    private char playerColour;
+
+
+    public RandomPlayer() {
+
+    }
+    
+    public void getPlay(){
+    	
+    	
+    }
+}
