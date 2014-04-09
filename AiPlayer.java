2e63f5eef7456420e48b566ca22190f889e5ad4d
Diarmuid Ryan <diarmuidr3d@gmail.com>
2014-04-09 14:26:48 +0100

    Added code for Assignment 5

---
+++ AiPlayer.java
@@ -0,0 +1,57 @@
+//
+// Team Name:
+// Version:
+//
+
+import java.util.ArrayList;
+
+public class AiPlayer {
+
+	private int playerId;
+	private Board gameBoard;
+	private Dice gameDice;
+	
+	
+	AiPlayer (int setPlayerId, Board setBoard, Dice setDice) {
+		playerId = setPlayerId;
+		gameBoard = setBoard;
+		gameDice = setDice;
+	    return;
+	}
+	
+	
+	public int getPlayerId () {
+		return playerId;
+	}
+	
+	
+	private int findBestBoard (ArrayList<Board> allBoardsList) {
+		int bestBoard = 0;
+		return bestBoard;
+	}
+	
+	
+	public Play getPlay () {
+		ArrayList<Play> allPlayList;
+		ArrayList<Board> allBoardsList = new ArrayList<Board>();
+		int bestBoard;
+		Play chosenPlay;
+		
+		allPlayList = gameBoard.allPossiblePlays (playerId, gameDice);
+		if (!allPlayList.isEmpty()) {
+			for (int i=0; i<allPlayList.size(); i++) {
+				allBoardsList.add(new Board(gameBoard));
+				allBoardsList.get(i).doPlay(playerId, allPlayList.get(i));
+			}
+			bestBoard = findBestBoard(allBoardsList);
+			chosenPlay = allPlayList.get(bestBoard);
+		}
+		else {
+			chosenPlay = new Play();
+		}
+		
+		return chosenPlay;
+	}
+		
+	
+}
