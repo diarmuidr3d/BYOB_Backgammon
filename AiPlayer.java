fc1fff10046dde43f5e4cd155022a86f78f1236a
Stefano <stefano.forti92@gmail.com>
2014-04-11 09:42:36 +0100

    Overall Design for AI

--- AiPlayer.java
+++ AiPlayer.java
@@ -25,7 +25,58 @@ public class AiPlayer {
 	public int getPlayerId () {
 		return playerId;
 	}
-	
+		private int blotEval(Board b){
+            return 0;
+        }
+        
+        private int blockEval(Board b){
+            return 0;
+        }
+        
+        private int runEval(Board b){
+            return 0;
+        }
+        
+        private int bearOffEval(Board b){
+            return 0;
+        }
+        
+        private int spacingEval(Board b){
+            return 0;
+        }
+        
+        private int computeHeuristic(Board b){
+            int heuristicScore, blotScore, blockScore, runScore, bearOffScore, spacingScore;
+            
+            blotScore = this.blotEval(b);
+            blockScore = this.blockEval(b);
+            runScore = this.runEval(b);
+            bearOffScore = this.bearOffEval(b);
+            spacingScore = this.spacingEval(b);
+            
+            heuristicScore = blotScore + blockScore + runScore + bearOffScore + spacingScore;
+            
+            return heuristicScore;
+        }
+        
+	private int findBestBoard (ArrayList<Board> allBoardsList) {
+		int bestBoard = 0, i = 0, max = 0, iMax = 0;
+                int[] boardScores = new int[allBoardsList.size()];
+                
+                for( Board b : allBoardsList ){
+                    boardScores[i] = this.computeHeuristic(b);
+                    i++;
+                }
+                
+                for (i=0; i<boardScores.length; i++){
+                    if (boardScores[i]>max){
+                        max = boardScores[i];
+                        iMax = i;
+                    }
+                }
+                
+		return iMax;
+	}
 	
 	private int findBestBoard (ArrayList<Board> allBoardsList) {
 		int bestBoard = 0;
