937d67fbc9d61d9f1f335e9bbd2e431bcab3adf2
Diarmuid Ryan <diarmuidr3d@gmail.com>
2014-04-14 09:14:56 +0100

    Updated blockEval method

--- AiPlayer.java
+++ AiPlayer.java
@@ -6,6 +6,7 @@ package backgammon;
 //
 
 import java.util.ArrayList;
+import java.util.PriorityQueue;
 
 public class AiPlayer {
 
@@ -30,60 +31,77 @@ public class AiPlayer {
     	if (playerId == Board.O_PLAYER_ID) return Board.X_PLAYER_ID;
     	else return Board.O_PLAYER_ID;
     }
-	
-	private int blotEval(Board b){
-            int playerBlots=0, adversaryBlots=0, score;
-            
-            for(int i = 0; i < Board.NUM_PIPS; i++){
-                if (b.checkers[this.getAdversaryId()][i] == 1) adversaryBlots++;
-                else if (b.checkers[this.getPlayerId()][i] == 1) playerBlots++;
-            }
-            
-            score = adversaryBlots - playerBlots;
-            
-            System.out.println("blotScore: " + adversaryBlots + " - " + playerBlots + " = " + score);
-            
-            return score;
-        }
-        
-        private int blockEval(Board b){
-            ArrayList<Play> plays = b.allPossiblePlays(getPlayerId(), gameDice);
-            Play p = new Play();
-            for (int i = 0; i < plays.size(); i++) {
-            	p = plays.get(i);
-            	for (int j = 0; j < p.length(); j++)
-            		p.getMove(j).getFromPip()
-            }
-        	return 0;
-        }
-        
-        private int runEval(Board b){
-            return 0;
-        }
-        
-        private int bearOffEval(Board b){
-            return 0;
-        }
-        
-        private int spacingEval(Board b){
-            return 0;
-        }
-        
-        private int computeHeuristic(Board b){
-            int heuristicScore, blotScore, blockScore, runScore, bearOffScore, spacingScore;
-            
-            blotScore = this.blotEval(b);
-            blockScore = this.blockEval(b);
-            runScore = this.runEval(b);
-            bearOffScore = this.bearOffEval(b);
-            spacingScore = this.spacingEval(b);
-            
-            heuristicScore = blotScore + blockScore + runScore + bearOffScore + spacingScore;
-            
-            return heuristicScore;
-        }
-        
-	private int findBestBoard (ArrayList<Board> allBoardsList) {
+
+    private int blotEval(Board b){
+    	int playerBlots=0, adversaryBlots=0, score;
+
+    	for(int i = 0; i < Board.NUM_PIPS; i++){
+    		if (b.checkers[this.getAdversaryId()][i] == 1) adversaryBlots++;
+    		else if (b.checkers[this.getPlayerId()][i] == 1) playerBlots++;
+    	}
+
+    	score = adversaryBlots - playerBlots;
+
+    	System.out.println("blotScore: " + adversaryBlots + " - " + playerBlots + " = " + score);
+
+    	return score;
+    }
+    
+    private float blockEval(Board b) {
+    	float blocksInRow = 0;
+    	float blockScore = 0;
+    	boolean opponentFoundOverAll = false, opponentFoundThisTurn;
+    	float weight = (float) 0.50;
+    	float retVal;
+    	for (int i = 1; i < 25; i++) {
+    		opponentFoundThisTurn = false;
+    		if (b.checkers[b.opposingPlayer(getPlayerId())][i] > 0) {
+    			weight = 1;
+    			opponentFoundThisTurn = true;
+    		}
+    		if (!opponentFoundThisTurn) {
+    			if (weight == 0.5) {
+    				if (blocksInRow < 3) {
+    					blocksInRow += weight;
+    				}
+    			} else if (blocksInRow < 6){
+    				blocksInRow += weight;
+    			}
+    		} else {
+    			blockScore += blocksInRow;
+    			blocksInRow = 0;
+    		}
+    	}
+    	return blockScore;
+    }
+
+    private int runEval(Board b){
+    	return 0;
+    }
+
+    private int bearOffEval(Board b){
+    	return 0;
+    }
+
+    private int spacingEval(Board b){
+    	return 0;
+    }
+
+    private int computeHeuristic(Board b){
+    	int heuristicScore, blotScore, blockScore, runScore, bearOffScore, spacingScore;
+
+    	blotScore = this.blotEval(b);
+    	blockScore = this.blockEval(b);
+    	runScore = this.runEval(b);
+    	bearOffScore = this.bearOffEval(b);
+    	spacingScore = this.spacingEval(b);
+
+    	heuristicScore = blotScore + blockScore + runScore + bearOffScore + spacingScore;
+
+    	return heuristicScore;
+    }
+
+    private int findBestBoard (ArrayList<Board> allBoardsList) {
 		int bestBoard = 0, i = 0, max;
                 int size = allBoardsList.size();
                 int[] boardScores = new int[size];
